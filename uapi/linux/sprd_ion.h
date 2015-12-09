5c12c3a9b640121366bf5d47460993ced01c7198
Wenan Hu <wenan.hu@spreadtrum.com>
2015-12-09 17:33:39 +0800

    Bug #510787 add sprd_ion.h
    [root cause  ] add sprd_ion.h
    [changes     ] add sprd_ion.h
    [side effects] none
    [self test   ]<9830, test ok>:
    [reviewers   ] Patrick.Shi, Michael.W
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I36aa77242473b85e86f852111b1f4414953385bd

---
+++ uapi/linux/sprd_ion.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2012 Spreadtrum Communications Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _UAPI_SPRD_ION_H
+#define _UAPI_SPRD_ION_H
+
+#define ION_HEAP_ID_SYSTEM   0
+#define ION_HEAP_ID_MM       1
+#define ION_HEAP_ID_OVERLAY  2
+#define ION_HEAP_ID_FB       3
+
+#define ION_HEAP_ID_MASK_SYSTEM        (1<<ION_HEAP_ID_SYSTEM)
+#define ION_HEAP_ID_MASK_MM            (1<<ION_HEAP_ID_MM)
+#define ION_HEAP_ID_MASK_OVERLAY       (1<<ION_HEAP_ID_OVERLAY)
+#define ION_HEAP_ID_MASK_FB            (1<<ION_HEAP_ID_FB)
+
+enum ION_SPRD_CUSTOM_CMD {
+	ION_SPRD_CUSTOM_PHYS = 0,
+	ION_SPRD_CUSTOM_MSYNC,
+	ION_SPRD_CUSTOM_FENCE_CREATE,
+	ION_SPRD_CUSTOM_FENCE_SIGNAL,
+	ION_SPRD_CUSTOM_FENCE_DUP,
+	ION_SPRD_CUSTOM_MAP_KERNEL,
+	ION_SPRD_CUSTOM_UNMAP_KERNEL,
+	ION_SPRD_CUSTOM_INVALIDATE,
+};
+
+enum SPRD_DEVICE_SYNC_TYPE {
+	SPRD_DEVICE_PRIMARY_SYNC,
+	SPRD_DEVICE_VIRTUAL_SYNC,
+};
+
+struct ion_phys_data {
+	int fd_buffer;
+	unsigned long phys;
+	size_t size;
+};
+
+struct ion_msync_data {
+	int fd_buffer;
+	unsigned long vaddr;
+	unsigned long paddr;
+	size_t size;
+};
+
+struct ion_fence_data {
+	uint32_t device_type;
+	int life_value;
+	int release_fence_fd;
+	int retired_fence_fd;
+};
+
+struct ion_kmap_data {
+	int fd_buffer;
+	uint64_t kaddr;
+	size_t size;
+};
+
+struct ion_kunmap_data {
+	int fd_buffer;
+};
+
+#endif /* _UAPI_SPRD_ION_H */
