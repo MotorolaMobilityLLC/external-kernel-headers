9c6ad6ea872bff70a5fde19506dffe6407409c6e
Jiao Feng <jiao.feng@spreadtrum.com>
2018-01-22 13:02:03 +0800

    Bug #901299 add camera reserved memory
    [root cause ] add camera reserved memory
    [changes ] add camera reserved memory
    [side effects] no
    [self test ]: 9850ka
    [download normally]:9850ka
    [power on/off normally]:9850ka
    [do common repository/branch inspection]:9850ka
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9850ka
    [test case]:9850ka
    [reviewers ] wenan.hu
    [change_type ] AOB --> sprd_adaptation
    [tag_product ] common
    
    Change-Id: Ica53c782422a90f7ebcded3f5c0ec470bc30449a
    (cherry picked from commit 3971b7d91c468b6b1c793b9e130aa9c7e16e594f)
    (cherry picked from commit b9626502c70b0c38f32d2ff845ce2529f9aace18)
    (cherry picked from commit 9a61fbd7088c238e3663cd7ca661e28b32a8d9a6)

---
+++ sprd_map.h
@@ -0,0 +1,14 @@
+#define SPRD_MAP_IOCTRL_MAGIC        'o'
+
+/**
+ * DOC: MAP_USER_VIR
+ *
+ * Takes a pmem_info struct
+ */
+#define MAP_USER_VIR    _IOWR(SPRD_MAP_IOCTRL_MAGIC, 0, struct pmem_info)
+
+struct pmem_info {
+    unsigned long phy_addr;
+    size_t size;
+};
+
