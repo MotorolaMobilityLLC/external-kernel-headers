7256295d3ded6aab6f0fee54b0312feae3fc44ba
chen.he <chen.he@spreadtrum.com>
2018-01-23 19:58:39 +0800

    Bug #707004 [sharkl3] GSP R6P0 adapt IFBC header
    [root cause  ] Add gsp r6p0 IFBC header
    [changes     ] gsp hal header
    [side effects]no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl3
    [test case]:compile pass
    [reviewers   ] tim.xia
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I53caf23d88d8109a72ced0ce59198dd6f7847fb7
    (cherry picked from commit 42cd5dab3fa75adec39b26e251a04607649347ca)

--- gsp_r6p0_cfg.h
+++ gsp_r6p0_cfg.h
@@ -126,6 +126,7 @@ struct gsp_r6p0_img_layer_params {
   __u8 y2y_mod;
   struct gsp_yuv_adjust_para yuv_adjust;
   struct gsp_scale_para scale_para;
+  __u32   header_size_r;
 };
 
 struct gsp_r6p0_img_layer_user {
@@ -150,6 +151,7 @@ struct gsp_r6p0_osd_layer_params {
   __u8 pmargb_en;
   __u8 pmargb_mod;
   __u8 zorder;
+  __u32   header_size_r;
 };
 
 struct gsp_r6p0_osd_layer_user {
@@ -167,6 +169,7 @@ struct gsp_r6p0_des_layer_params {
   __u8 fbc_mod;
   __u8 dither_en;
   struct gsp_background_para bk_para;
+  __u32   header_size_r;
 };
 
 struct gsp_r6p0_des_layer_user {
