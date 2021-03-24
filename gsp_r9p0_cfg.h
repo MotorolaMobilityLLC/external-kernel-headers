cf94077da1b0dcb5749b21cab2c9992cfeb0c5f2
hanguanghui <guanghui.han@unisoc.com>
2021-03-24 15:00:08 +0800

    Bug #1545198 add gsp secure support
    Bug #1545198 add gsp secure support
    
    [root cause  ] add gsp secure support
    [changes     ] gsp_r9p0_cfg.h
    [side effects] no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9620
    [test case]:compile pass
    [reviewers   ] brooke.xu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I79bd0662d55eef0343d5f4b99762e4b14998c177

--- gsp_r9p0_cfg.h
+++ gsp_r9p0_cfg.h
@@ -127,7 +127,6 @@ struct gsp_r9p0_img_layer_params {
   struct gsp_yuv_adjust_para yuv_adjust;
   struct gsp_scale_para scale_para;
   __u32   header_size_r;
-  uint8_t secure_en;
 };
 
 struct gsp_r9p0_img_layer_user {
@@ -153,7 +152,6 @@ struct gsp_r9p0_osd_layer_params {
   __u8 pmargb_mod;
   __u8 zorder;
   __u32   header_size_r;
-  uint8_t secure_en;
 };
 
 struct gsp_r9p0_osd_layer_user {
@@ -188,7 +186,6 @@ struct gsp_r9p0_misc_cfg_user {
   uint8_t pmargb_en;
   struct gsp_rect workarea_src_rect;
   struct gsp_pos workarea_des_pos;
-  uint8_t secure_en;
 };
 
 struct gsp_r9p0_cfg_user {
